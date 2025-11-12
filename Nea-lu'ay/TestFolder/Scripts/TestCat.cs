e2f8114db442156f22d0ffffb78a167d4dde0857
Lu'ay <luaylamia@gmail.com>
2025-11-12 08:23:52 +0000

    Best to revert, and make a cat manager for boids.

--- Nea-lu'ay/TestFolder/Scripts/TestCat.cs
+++ Nea-lu'ay/TestFolder/Scripts/TestCat.cs
@@ -6,10 +6,12 @@ public partial class TestCat : CharacterBody3D
 	
 	private CharacterBody3D _player;
 	private CharacterBody3D _catBody;
+	private Vector3 velocity;
 	
 	public override void _Ready()
 	{
 		_player = GetNode<CharacterBody3D>("%fpsCamera");
+		velocity = Vector3.Zero;
 	}
 	
 	public override void _PhysicsProcess(double delta)
@@ -19,7 +21,11 @@ public partial class TestCat : CharacterBody3D
 		var targetTransform = this.Transform.LookingAt(targetPos, Vector3.Up);
 		this.Transform = this.Transform.InterpolateWith(targetTransform, 5 * (float)delta);
 		
-		/*Velocity = MoveToward(_player.GetGlobalPosition(), delta);
-		MoveAndSlide();*/
+		velocity.X = Mathf.MoveToward(this.GetGlobalPosition().X, _player.GetGlobalPosition().X, (float)delta);
+		velocity.Z = Mathf.MoveToward(this.GetGlobalPosition().Z, _player.GetGlobalPosition().Z, (float)delta);
+		velocity.Y = Mathf.MoveToward(this.GetGlobalPosition().Y, _player.GetGlobalPosition().Y, (float)delta);
+		
+		Velocity = velocity;
+		MoveAndSlide();
 	}
 }
