164dc77e15f327088e91f7d26149ecc7ad6017c4
Lu'ay <luaylamia@gmail.com>
2025-11-19 08:22:20 +0000

    Rule 3 has been improved, Rule 2 actually works

--- Nea-lu'ay/TestFolder/Scripts/TestCat.cs
+++ Nea-lu'ay/TestFolder/Scripts/TestCat.cs
@@ -7,11 +7,11 @@ public partial class TestCat : CharacterBody3D
 	private CharacterBody3D _player;
 	private CharacterBody3D _catBody;
 	private Vector3 velocity;
-	
+		
 	public override void _Ready()
 	{
 		_player = GetNode<CharacterBody3D>("%fpsCamera");
-		//velocity = Vector3.Zero;
+		velocity = Vector3.Zero;
 	}
 	
 	public override void _PhysicsProcess(double delta)
@@ -21,11 +21,11 @@ public partial class TestCat : CharacterBody3D
 		var targetTransform = this.Transform.LookingAt(targetPos, Vector3.Up);
 		this.Transform = this.Transform.InterpolateWith(targetTransform, 5 * (float)delta);
 		
-		/*velocity.X = Mathf.MoveToward(this.GetGlobalPosition().X, _player.GetGlobalPosition().X, (float)delta);
-		velocity.Z = Mathf.MoveToward(this.GetGlobalPosition().Z, _player.GetGlobalPosition().Z, (float)delta);
-		velocity.Y = Mathf.MoveToward(this.GetGlobalPosition().Y, _player.GetGlobalPosition().Y, (float)delta);
+		if (!IsOnFloor())
+		{
+			Velocity += GetGravity() * (float)delta * 1.3f;// fine tune gravity.
+		}
 		
-		Velocity = velocity;
-		MoveAndSlide();*/
+		MoveAndSlide();//this means the manager won't need to move this.
 	}
 }
--- Nea-lu'ay/TestFolder/TestCatManager.cs
+++ Nea-lu'ay/TestFolder/TestCatManager.cs
@@ -10,20 +10,18 @@ public partial class TestCatManager : Node
 	{
 		targetBoid = GetNode<CharacterBody3D>("%fpsCamera");
 		SetBoidArray();
-		GD.Print("boidArray: " + boidArray);
+		//GD.Print("boidArray: " + boidArray);
 	}
 	
 	public void SetBoidArray()
 	{
 		Godot.Collections.Array<Node> tempArray = GetTree().GetNodesInGroup("Cat");
 		boidArray.Clear();
-		GD.Print("tempArray: " + tempArray);
 		for (int i = 0; i < tempArray.Count; i++)
 		{
 			string nodePath = (string)tempArray[i].GetPath();
-			GD.Print("tempArray: " + tempArray[i] + ", node path: " + nodePath + ", i: " + i);
+			//GD.Print("tempArray: " + tempArray[i] + ", node path: " + nodePath + ", i: " + i);
 			boidArray.Add(GetNode<CharacterBody3D>(nodePath));
-			GD.Print("boidArray: " + boidArray + ", i: " + i);
 		}
 	}
 	
@@ -31,28 +29,33 @@ public partial class TestCatManager : Node
 	{
 		Vector3 targetPos = targetBoid.GetGlobalPosition();
 		Vector3 targetVelocity = targetBoid.Velocity;
+		Vector3 finalVelocity = Vector3.Zero;
 		
 		for (int i = 0; i < boidArray.Count; i++)
 		{
-			Vector3 centerCorrection = Rule1(boidArray[i].GetGlobalPosition(), targetPos);
-			GD.Print("Rule 1, Boid:" + i + " ," + centerCorrection);
-			Vector3 objectAvoidence = Rule2(boidArray[i]);
-			GD.Print("Rule 2, Boid:" + i + " ," + objectAvoidence);
+			Vector3 currentPos = boidArray[i].GetGlobalPosition();
+			//GD.Print(currentPos);
+			
+			Vector3 centerCorrection = Rule1(currentPos, targetPos);
+			//GD.Print("Rule 1, Boid:" + i + " ," + centerCorrection);
+			Vector3 objectAvoidence = Rule2(boidArray[i], currentPos);
+			//GD.Print("Rule 2, Boid:" + i + " ," + objectAvoidence);
 			Vector3 velocityCorrection = Rule3(boidArray[i], targetVelocity);
-			GD.Print("Rule 3, Boid:", i + " ," + velocityCorrection);
+			//GD.Print("Rule 3, Boid:", i + " ," + velocityCorrection);
+			
+			finalVelocity = centerCorrection + objectAvoidence + velocityCorrection;
+			//GD.Print("Boid ", i, "velocity: ", finalVelocity);
+			boidArray[i].Velocity = finalVelocity;
 		}
-		
-		/*Velocity = 
-		MoveAndSlide();*/
 	}
 	
 	public Vector3 Rule1(Vector3 boidPos, Vector3 targetPos)
 	{
 		//I might want to touch the Y magnatude, as this might cause them to fly...
-		return new Vector3 ((targetPos.X-boidPos.X)/100, (targetPos.Y-boidPos.Y)/100, (targetPos.Z-boidPos.Z)/100);
+		return new Vector3 (((targetPos.X-boidPos.X)*0.7f), ((targetPos.Y-boidPos.Y)*0.7f), ((targetPos.Z-boidPos.Z)*0.7f));
 	}
 	
-	public Vector3 Rule2(CharacterBody3D boid)
+	public Vector3 Rule2(CharacterBody3D boid, Vector3 boidPos) //I don't think this does enough...
 	{
 		Vector3 avoidance = Vector3.Zero;
 		
@@ -60,7 +63,11 @@ public partial class TestCatManager : Node
 		{
 			if(boid != boidArray[i])
 			{
-				avoidance -= boid.GetGlobalPosition() - boidArray[i].GetGlobalPosition();
+				Vector3 temp = boidArray[i].GetGlobalPosition();
+				if ((Mathf.Sqrt(Mathf.Pow((boidPos.X+temp.X), 2)+Mathf.Pow((boidPos.Z+temp.Z), 2))) < 0.25f)
+				{
+					avoidance -= boidPos - temp;
+				}
 			}
 		}
 		
@@ -69,7 +76,7 @@ public partial class TestCatManager : Node
 	
 	public Vector3 Rule3(CharacterBody3D boid, Vector3 targetVelocity)
 	{
-		Vector3 newVelocity = targetVelocity;
+		Vector3 newVelocity = Vector3.Zero;
 		
 		for(int i = 0; i < boidArray.Count; i++)
 		{
