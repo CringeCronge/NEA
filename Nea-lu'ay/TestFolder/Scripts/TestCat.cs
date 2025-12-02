ab2971f6a942709856c1c2e069f34365372d3a85
Lu'ay <luaylamia@gmail.com>
2025-12-02 12:45:35 +0000

    All three boid rules work, might fine tune and add new ones

--- Nea-lu'ay/TestFolder/Scripts/TestCat.cs
+++ Nea-lu'ay/TestFolder/Scripts/TestCat.cs
@@ -6,11 +6,14 @@ public partial class TestCat : CharacterBody3D
 	
 	private CharacterBody3D _player;
 	private CharacterBody3D _catBody;
+	private RayCast3D _groundCast;
+
 	private Vector3 velocity;
 		
 	public override void _Ready()
 	{
 		_player = GetNode<CharacterBody3D>("%fpsCamera");
+		//_groundCast = GetNode<RayCast3D>("CollisionShape3D/GroundCast");
 		velocity = Vector3.Zero;
 	}
 	
@@ -21,11 +24,9 @@ public partial class TestCat : CharacterBody3D
 		var targetTransform = this.Transform.LookingAt(targetPos, Vector3.Up);
 		this.Transform = this.Transform.InterpolateWith(targetTransform, 5 * (float)delta);
 		
-		if (!IsOnFloor())
-		{
-			Velocity += GetGravity() * (float)delta * 1.3f;// fine tune gravity.
-		}
 		
+		
+		Velocity += velocity;
 		MoveAndSlide();//this means the manager won't need to move this.
 	}
 }
--- Nea-lu'ay/TestFolder/Scripts/TestCatManager.cs
+++ Nea-lu'ay/TestFolder/Scripts/TestCatManager.cs
@@ -33,29 +33,36 @@ public partial class TestCatManager : Node
 		
 		for (int i = 0; i < boidArray.Count; i++)
 		{
-			Vector3 currentPos = boidArray[i].GetGlobalPosition();
+			Vector3 currentPos = boidArray[i].GetGlobalPosition() + new Vector3(0, 0.5f, 0);
 			//GD.Print(currentPos);
 			
 			Vector3 centerCorrection = Rule1(currentPos, targetPos);
 			//GD.Print("Rule 1, Boid:" + i + " ," + centerCorrection);
-			Vector3 objectAvoidence = Rule2(boidArray[i], currentPos);
-			//GD.Print("Rule 2, Boid:" + i + " ," + objectAvoidence);
+			Vector3 objectAvoidence = Rule2(boidArray[i], currentPos, targetPos);
+			GD.Print("Rule 2, Boid:" + i + " ," + objectAvoidence);
 			Vector3 velocityCorrection = Rule3(boidArray[i], targetVelocity);
 			//GD.Print("Rule 3, Boid:", i + " ," + velocityCorrection);
 			
-			finalVelocity = centerCorrection + objectAvoidence + velocityCorrection;
-			//GD.Print("Boid ", i, "velocity: ", finalVelocity);
+			finalVelocity =  centerCorrection + objectAvoidence + velocityCorrection;
 			boidArray[i].Velocity = finalVelocity;
+			
 		}
 	}
 	
+	///<summary>This provides an updated postion for centre correction.</summary>
 	public Vector3 Rule1(Vector3 boidPos, Vector3 targetPos)
 	{
-		//I might want to touch the Y magnatude, as this might cause them to fly...
-		return new Vector3 (((targetPos.X-boidPos.X)*0.7f), ((targetPos.Y-boidPos.Y)*0.7f), ((targetPos.Z-boidPos.Z)*0.7f));
+		return new Vector3 (((targetPos.X-boidPos.X)*0.5f), ((targetPos.Y-boidPos.Y)), ((targetPos.Z-boidPos.Z)*0.5f));
 	}
 	
-	public Vector3 Rule2(CharacterBody3D boid, Vector3 boidPos) //I don't think this does enough...
+	/// <summary>
+	/// This checks if another boid is within 25cm, and will provide an updated boid to avoid them.
+	/// </summary>
+	/// <param name="boid"></param>
+	/// <param name="boidPos"></param>
+	/// <param name="playerPos"></param>
+	/// <returns></returns>
+	public Vector3 Rule2(CharacterBody3D boid, Vector3 boidPos, Vector3 playerPos) //I don't think this does enough...
 	{
 		Vector3 avoidance = Vector3.Zero;
 		
@@ -64,19 +71,34 @@ public partial class TestCatManager : Node
 			if(boid != boidArray[i])
 			{
 				Vector3 temp = boidArray[i].GetGlobalPosition();
-				if ((Mathf.Sqrt(Mathf.Pow((boidPos.X+temp.X), 2)+Mathf.Pow((boidPos.Z+temp.Z), 2))) < 0.25f)
+				
+				if(Mathf.Sqrt(Mathf.Pow((boidPos.X-temp.X), 2)+Mathf.Pow((boidPos.Z-temp.Z), 2)) < 0.6f)
 				{
-					avoidance -= boidPos - temp;
+					avoidance = avoidance - boidPos - temp;
 				}
 			}
+			
 		}
+		
+		if(Mathf.Sqrt(Mathf.Pow((boidPos.X-playerPos.X), 2)+Mathf.Pow((boidPos.Z-playerPos.Z), 2)) < 2)
+		{
+			avoidance = 0.3f*(avoidance - boidPos - playerPos);
+		}
+		
+		avoidance.Y = 0;
 		
 		return avoidance;
 	}
 	
-	public Vector3 Rule3(CharacterBody3D boid, Vector3 targetVelocity)
+	/// <summary>
+	/// This matches boid velocity with other boid velocities.
+	/// </summary>
+	/// <param name="boid"></param>
+	/// <param name="playerVelocity"></param>
+	/// <returns></returns>
+	public Vector3 Rule3(CharacterBody3D boid, Vector3 playerVelocity)
 	{
-		Vector3 newVelocity = Vector3.Zero;
+		Vector3 newVelocity = new Vector3 (0f, playerVelocity.Y, 0f);
 		
 		for(int i = 0; i < boidArray.Count; i++)
 		{
@@ -87,5 +109,18 @@ public partial class TestCatManager : Node
 		}
 		
 		return ((newVelocity/(boidArray.Count-1))-boid.Velocity)/8.0f;
+	}
+
+	/// <summary>
+	/// This is just a place holder.
+	/// </summary>
+	/// <param name="boid"></param>
+	/// <param name="playerPos"></param>
+	/// <returns></returns>
+	public Vector3 Rule4(CharacterBody3D boid, Vector3 playerPos)
+	{
+		
+		
+		return Vector3.Zero;
 	}
 }
